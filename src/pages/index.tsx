c55b30e4ae1015a3682d8da3791d89d2bef20d96
Sebastian Alsina <alsinas@me.com>
2021-05-11 17:49:51 -0400

    TextFix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,13 +48,13 @@ export default function App() {
                                     </div>
                                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
                                         <div>
-                                            <p className="text-2xl sm:text-4xl font-bold">
+                                            <div className="text-2xl sm:text-4xl font-bold">
                                                 <p className="inline rainbow-text">Scream is a AMM yield generator and lending protocol powered by </p>
                                                 <a href="https://fantom.foundation" target="_blank" rel="noreferrer" className="inline whitespace-nowrap hover:underline" style={{ color: '#13b5ec' }}>
                                                     <img className="inline-block align-middle h-6 mr-1" src="/img/fantom-logo.svg" alt="" />
                                                     fantom.
                                                 </a>
-                                            </p>
+                                            </div>
                                         </div>
                                     </div>
                                     <div className="text-xl space-x-2">
