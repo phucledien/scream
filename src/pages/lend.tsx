de0de67ed5d7a6cf86cced829862d2ffc18514b5
Sebastian Alsina <alsinas@me.com>
2021-07-10 14:23:06 -0400

    Push!

--- src/pages/lend.tsx
+++ src/pages/lend.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import AssetTable from '../components/AssetTable'
 import BorrowTool from '../components/BorrowTool'
 import Footer from '../components/Footer'
@@ -6,30 +7,31 @@ import TokenPercentageBar from '../components/TokenPercentageBar'
 import LiqudityTable from '../components/LiqudityTable'
 import Stats from '../components/Stats'
 import Header from '../components/Header'
-import { useState } from 'react'
 import useMarkets from '../hooks/useMarkets'
+
+//
 
 export default function App() {
     const [refreshMarket, setRefreshMarket] = useState(0)
 
-    const markets = useMarkets(refreshMarket);
+    const markets = useMarkets(refreshMarket)
 
     const update = () => {
-        setRefreshMarket(prev => prev + 1);
+        setRefreshMarket((prev) => prev + 1)
     }
 
     return (
         <>
             <Header />
-            <Stats markets={markets}/> 
+            <Stats markets={markets} />
             <div className="max-w-5xl mx-auto p-6 pb-12 md:p-12 md:pb-24">
                 <div className="space-y-6 md:space-y-12">
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:gap-12">
-                        <BorrowTool markets={markets} update={update}/>
-                        <LendTool markets={markets} update={update}/>
+                        <BorrowTool markets={markets} update={update} />
+                        <LendTool markets={markets} update={update} />
                     </div>
                     {/* <RepayTool /> */}
-                    <AssetTable markets={markets} update={update}/>
+                    <AssetTable markets={markets} update={update} />
                 </div>
             </div>
             {/* <div className="bg-pink-200">
