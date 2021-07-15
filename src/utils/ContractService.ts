2d82db80fa85421b01daa6d09c22d7f269d392b5
Sebastian Alsina <alsinas@me.com>
2021-07-15 15:47:38 -0400

    Linked Assets

--- src/utils/ContractService.ts
+++ src/utils/ContractService.ts
@@ -5,22 +5,22 @@ import * as constants from '../constants'
 export const simpleRpcProvider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_NETWORK_URL)
 
 const TOKEN_ABI = {
-  wftm: constants.CONTRACT_WFTM_TOKEN_ABI,
-  usdc: constants.CONTRACT_USDC_TOKEN_ABI,
-  dai: constants.CONTRACT_DAI_TOKEN_ABI,
-  scream: constants.CONTRACT_SCREAM_TOKEN_ABI,
-  fbtc:  constants.CONTRACT_FBTC_TOKEN_ABI,
-  btc:  constants.CONTRACT_WBTC_TOKEN_ABI,
-  eth:  constants.CONTRACT_WETH_TOKEN_ABI,
-  feth:  constants.CONTRACT_FETH_TOKEN_ABI,
-  fusdt:  constants.CONTRACT_FUSDT_TOKEN_ABI,
-  sushi:  constants.CONTRACT_SUSHI_TOKEN_ABI,
-  band:  constants.CONTRACT_BAND_TOKEN_ABI,
-  yfi:  constants.CONTRACT_YFI_TOKEN_ABI,
-  crv:  constants.CONTRACT_CRV_TOKEN_ABI,
-  frax:  constants.CONTRACT_FRAX_TOKEN_ABI,
-  snx:  constants.CONTRACT_SNX_TOKEN_ABI,
-};
+    wftm: constants.CONTRACT_WFTM_TOKEN_ABI,
+    usdc: constants.CONTRACT_USDC_TOKEN_ABI,
+    dai: constants.CONTRACT_DAI_TOKEN_ABI,
+    scream: constants.CONTRACT_SCREAM_TOKEN_ABI,
+    fbtc: constants.CONTRACT_FBTC_TOKEN_ABI,
+    btc: constants.CONTRACT_WBTC_TOKEN_ABI,
+    eth: constants.CONTRACT_WETH_TOKEN_ABI,
+    feth: constants.CONTRACT_FETH_TOKEN_ABI,
+    fusdt: constants.CONTRACT_FUSDT_TOKEN_ABI,
+    sushi: constants.CONTRACT_SUSHI_TOKEN_ABI,
+    band: constants.CONTRACT_BAND_TOKEN_ABI,
+    yfi: constants.CONTRACT_YFI_TOKEN_ABI,
+    crv: constants.CONTRACT_CRV_TOKEN_ABI,
+    frax: constants.CONTRACT_FRAX_TOKEN_ABI,
+    snx: constants.CONTRACT_SNX_TOKEN_ABI
+}
 
 export const getContract = (address: string, abi: any, signer?: ethers.Signer | ethers.providers.Provider) => {
     const signerOrProvider = signer ?? simpleRpcProvider
@@ -47,20 +47,18 @@ export const getInterestModelContract = (address, provider?: ethers.Signer | eth
 export const getMulticallContract = (provider?: ethers.Signer | ethers.providers.Provider) => getContract(constants.CONTRACT_MULTICALL_ADDRESS, JSON.parse(constants.CONTRACT_MULTICALL_ABI), provider)
 
 export const fetchBalances = async (account: string, token: any, scToken: any, provider?: ethers.Signer | ethers.providers.Provider) => {
-  if(token && scToken) {
-    const tokenContract = getTokenContract(token.id, provider);
-    const scTokenContract = getSctokenContract(scToken.id, provider); 
-    const appContract = getUnitrollerContract(provider);
-    const tokenDecimal = +token.decimals;
-    
-    const [wallet, allow, snapshot, balance] = await Promise.all([
-      tokenContract.balanceOf(account),
-      tokenContract.allowance(account, scToken.address),
-      scTokenContract.getAccountSnapshot(account),
-      scTokenContract.balanceOf(account),
-    ]);
+    if (token && scToken) {
+        const tokenContract = getTokenContract(token.id, provider)
+        const scTokenContract = getSctokenContract(scToken.id, provider)
+        const appContract = getUnitrollerContract(provider)
+        const tokenDecimal = +token.decimals
 
-    
+        const [wallet, allow, snapshot, balance] = await Promise.all([
+            tokenContract.balanceOf(account),
+            tokenContract.allowance(account, scToken.address),
+            scTokenContract.getAccountSnapshot(account),
+            scTokenContract.balanceOf(account)
+        ])
 
         const hypotheticalLiquidity = await appContract.getHypotheticalAccountLiquidity(account, scToken.address, balance, 0)
 
