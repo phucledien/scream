58932aac249abfa2f2a3c3bd6f0f881bdc08cbad
Sebastian Alsina <alsinas@me.com>
2021-07-19 14:57:58 -0400

    Added Icons

--- src/components/TokenPercentageBar.tsx
+++ src/components/TokenPercentageBar.tsx
@@ -10,7 +10,8 @@ interface TokenPercentageBarProps {
 export default function TokenPercentageBar({ src, name, value, percentage }: TokenPercentageBarProps) {
     return (
         <div className="flex items-center space-x-2">
-            <div className="h-6 w-6 bg-gray-100 rounded-full" />
+            <img className="h-4 w-4 bg-gray-100 rounded-full" src={src} alt="" />
+            {/* <div className="h-6 w-6 bg-gray-100 rounded-full" /> */}
             <p className="text-xs font-bold">{name}</p>
             <Progress className="flex-1" type="secondary" value={value} />
             <p className="text-xs font-medium">{value}%</p>
