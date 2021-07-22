404c2209c3c3a61e671902e86a3ae4460e1731fa
xam-darnold <xamcrypto@gmail.com>
2021-07-22 10:13:22 -0500

    Change useReward formatting

--- src/hooks/useRewards.tsx
+++ src/hooks/useRewards.tsx
@@ -9,20 +9,40 @@ export default function useRewards(tokenData) {
     const [borrowApy, setBorrowApy] = useState(0)
     const { account, library } = useActiveWeb3React()
 
-    useEffect(() => console.log(tokenData), [tokenData])
+    const calculateAPY = async (compSpeeds, scToken) => {
+        try {
+            const totalSupply = await scToken.totalSupply()
+            const totalBorrow = await scToken.totalBorrows()
+            // write hook to calculate scream price
+            const screamPrice = 1
+            const blocksPerDay = 86400 // 1 seconds per block
+            const daysPerYear = 365
+            const screamPerYear = compSpeeds.mul(blocksPerDay).mul(daysPerYear).mul(screamPrice)
+            const lendingAPY = screamPerYear.div(totalSupply).mul(100).toNumber()
+            const borrowAPY = screamPerYear.div(totalBorrow).mul(100).toNumber()
+            return { lendingAPY, borrowAPY }
+        } catch (error) {
+            console.log(error)
+            return { lendingApy: 0, borrowApy: 0 }
+        }
+    }
 
     useEffect(() => {
         if (account && tokenData) {
             const appContract = getUnitrollerContract(library?.getSigner())
             const fetchRewards = async () => {
-                const [compAccrued, compSpeeds, compRate] = await Promise.all([appContract.compAccrued(account), appContract.compSpeeds(tokenData.id), appContract.compRate()])
-                compAccrued ? setRewardValue(compAccrued.toNumber()) : setRewardValue(0)
+                const [compAccrued, compSpeeds] = await Promise.all([appContract.compAccrued(account), appContract.compSpeeds(tokenData.id)])
+                if (compAccrued) {
+                    setRewardValue(compAccrued.toNumber())
+                } else {
+                    setRewardValue(0)
+                }
+
                 const scToken = getSctokenContract(tokenData.underlyingSymbol.toLowerCase(), library)
 
                 const apys = await calculateAPY(compSpeeds, scToken)
-                console.log(apys.lendingApy)
-                setLendingApy(apys.lendingApy)
-                setBorrowApy(apys.borrowApy)
+                setLendingApy(apys.lendingAPY)
+                setBorrowApy(apys.borrowAPY)
             }
             fetchRewards()
         }
@@ -33,31 +53,11 @@ export default function useRewards(tokenData) {
             const appContract = getUnitrollerContract(library?.getSigner())
             let tx = null
             try {
-                console.log(appContract)
-                tx = await appContract["claimComp(address,address[])"](account, [tokenData.id])
+                tx = await appContract['claimComp(address,address[])'](account, [tokenData.id])
                 console.log(tx)
             } catch (error) {
                 console.log(error)
             }
-        }
-    }
-
-    const calculateAPY = async (compSpeeds, scToken) => {
-        try {
-            //have to unwrap and rewrap for BigNumber to work?
-            const totalSupply = await scToken.totalSupply()
-            const totalBorrow = await scToken.totalBorrows()
-            //write hook to calculate scream price
-            const screamPrice = 1
-            const blocksPerDay = 86400 // 1 seconds per block
-            const daysPerYear = 365
-            const screamPerYear = compSpeeds.mul(blocksPerDay).mul(daysPerYear).mul(screamPrice)
-            const lendingApy = screamPerYear.div(totalSupply).mul(100).toNumber()
-            const borrowApy = screamPerYear.div(totalBorrow).mul(100).toNumber()
-            return {lendingApy, borrowApy}
-        } catch (error) {
-            console.log(error)
-            return {lendingApy: 0, borrowApy: 0}
         }
     }
 
