600e09c2df5776566966ed3677a036f9cab8bb2a
xam-darnold <xamcrypto@gmail.com>
2021-08-03 04:47:10 -0500

    Remove console.log

--- src/components/APYStat.tsx
+++ src/components/APYStat.tsx
@@ -9,15 +9,11 @@ interface APYStat {
 export default function APYStat({ market, type }: APYStat) {
     const { lendingApy, borrowApy } = useRewards(market)
 
-    useEffect(() => {
-        console.log('market', market)
-    }, [market])
-
     return (
         <div>
             <p>{type == 'lend' ? market.supplyAPY?.toFixed(2) : market.borrowAPY?.toFixed(2)}%</p>
             <div className="flex items-center space-x-1">
-                <p className="text-xs font-mono opacity-50">{type == 'lend' ? lendingApy : borrowApy}%</p>
+                <p className="font-mono text-xs opacity-50">{type == 'lend' ? lendingApy : borrowApy}%</p>
                 <img className="w-3" src="/img/tokens/scream.png" alt="" />
             </div>
         </div>
